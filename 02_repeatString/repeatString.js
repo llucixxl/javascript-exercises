44ad90eebc05b68b4caba012025b3b98db0e3dec
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-05-27 18:53:43 +0300

    Add function to solve the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let severalGreeting = "";
 
+  if (num < 0) return "ERROR";
+
+  for (let i = 0; i < num; i++) {
+    severalGreeting += string;
+  }
+  return severalGreeting;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;

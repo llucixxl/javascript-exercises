fe52918f484455008f5a4ec74a306c018f98dca3
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-05-27 20:24:27 +0300

    Add my really complicated solution to solve the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function (input) {
+  let splitResult = input.split("").reverse();
+  let reversedString = "";
 
+  for (let i = 0; i < splitResult.length; i++) {
+    reversedString += splitResult[i];
+  }
+  return reversedString;
 };
-
+reverseString(`hello there`);
 // Do not edit below this line
 module.exports = reverseString;

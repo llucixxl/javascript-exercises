5e6c1c79c00bb50cc0e29817ab25911b9084b3f2
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-05-31 22:06:42 +0300

    Add code to solve the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (value1) {
+  let resultToCelsius;
+
+  resultToCelsius = (value1 - 32) * (5 / 9);
+  return Number(resultToCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(32));
+
+const convertToFahrenheit = function (value2) {
+  let resultToFahrenheit;
+
+  resultToFahrenheit = value2 * (9 / 5) + 32;
+  return Number(resultToFahrenheit.toFixed(1));
 };
+
+console.log(convertToFahrenheit(1));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

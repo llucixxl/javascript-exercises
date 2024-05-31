91bcd185a11bb64d690e75f7cbc40ac85483125e
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-05-31 18:50:39 +0300

    Add solution to solve the exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let leapYearAlert = "";
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) {
+    leapYearAlert = false;
+  } else if (year % 100 === 0 && year % 400 === 0) {
+    leapYearAlert = true;
+  } else if (year % 100 === 0 && year % 400 === 1) {
+    leapYearAlert = false;
+  } else if (year % 4 === 0) {
+    leapYearAlert = true;
+  } else {
+    leapYearAlert = false;
+  }
+  console.log(leapYearAlert);
+  return leapYearAlert;
 };
+
+leapYears(1600);
 
 // Do not edit below this line
 module.exports = leapYears;
